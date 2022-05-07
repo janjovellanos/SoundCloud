1d0cdb8594150c45a322f235987d730d8d1f557e
Jan <janjovellanos@gmail.com>
2022-05-07 13:59:59 -0700

    added new db img

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -5,10 +5,10 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser')
+const { ValidationError } = require('sequelize');
 
 const routes = require('./routes');
 
-const { ValidationError } = require('sequelize');
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
---
+++ authenticate-me/backend/utils/auth.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+
+//send a JWT cookie
+const setTokenCookie = (res, user) => {
+    //create token
+    const token = jwt.sign(
+        { data: user.toSafeObject() },
+        secret,
+        { expiresIn: parseInt(expiresIn) }
+    );
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    //set token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000,
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
+    });
+    return token;
+};
+
+const restoreUser = (req, res, next) => {
+    //token parsed from cookies
+    const { token } = req.cookies;
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
