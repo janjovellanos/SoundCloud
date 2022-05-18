c2b285ef1478608c8f4357496388eb93fbb6382c
Jan <janjovellanos@gmail.com>
2022-05-17 20:50:51 -0700

    login and get current user

---
+++ backend/routes/login.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { check } = require('express-validator');
+
+const { setTokenCookie, restoreUser } = require('../utils/auth');
+const { handleValidationErrors } = require('../utils/validation');
+const { User } = require('../db/models');
+
+const router = express.Router();
+
+//req body validation middleware
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+router.get('/1', async (req, res, next) => {
+    res.json({ 1: 'whatsup!' })
+})
+
+//login user
+router.post('/', validateLogin, async (req, res, next) => {
+    const { credential, password } = req.body;
+    //login static method
+    const user = await User.login({ credential, password });
+    if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid'];
+        return next(err);
+    }
+    await setTokenCookie(res, user);
+    return res.json({ user });
+});
+
+module.exports = router;
