ac66c8cb2d81c690e5b2b2facc6c72f5c04e8924
Jan <janjovellanos@gmail.com>
2022-05-17 20:23:49 -0700

    seeded

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
+const loginRouter = require('./api')
 
 router.use('/api', apiRouter);
+router.use('/login', loginRouter);
 
 //to get TOKEN while developing app
 router.get('/api/csrf/restore', (req, res) => {
