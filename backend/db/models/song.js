b230b7f05a14b3c52a97c09b8da4daf549288ff0
Jan <janjovellanos@gmail.com>
2022-05-18 21:05:16 -0700

    got song by id

--- backend/db/models/song.js
+++ backend/db/models/song.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Song.belongsTo(models.User, { foreignKey: 'userId' });
+      Song.belongsTo(models.User, { foreignKey: 'userId', as: 'Artist' });
       Song.belongsTo(models.Album, { foreignKey: 'albumId' });
       Song.hasMany(models.Comment, { foreignKey: 'songId' });
       Song.belongsToMany(models.Playlist, { through: models.PlaylistSong });
--- backend/routes/songs.js
+++ backend/routes/songs.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const { check } = require('express-validator');
 
-const { Song } = require('../db/models');
+const { Song, User, Album } = require('../db/models');
 
 const router = express.Router();
+
+router.get('/:songId', async (req, res) => {
+    const { songId } = req.params;
+
+    const song = await Song.findByPk(songId, {
+        include: [
+            { model: User, as: 'Artist', attributes: ['id', 'username'] },
+            { model: Album, attributes: ['id', 'title', 'imageUrl'] }
+        ]
+
+    });
+
+    res.json(song);
+})
 
 //get all songs
 router.get('/', async (req, res) => {
     const Songs = await Song.findAll();
     res.json({ Songs });
 })
+
 
 
 module.exports = router;
