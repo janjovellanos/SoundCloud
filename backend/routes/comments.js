6bbb26ccf1ccc5d810f540bd0b9fdc157e2a8a3d
Jan <janjovellanos@gmail.com>
2022-05-21 10:40:31 -0700

    success

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -39,4 +39,32 @@ router.put('/:commentId', requireAuth, validateCommentCreation, async (req, res,
     }
 });
 
+//delete comment
+router.delete('/:commentId', requireAuth, async (req, res, next) => {
+    const { commentId } = req.params;
+    const { user } = req;
+
+    const comment = await Comment.findByPk(commentId);
+
+    if (comment) {
+        if (comment.userId === user.id) {
+            await comment.destroy();
+            res.json({
+                message: 'Successfully deleted',
+                statusCode: 200
+            });
+        } else {
+            const err = new Error('Not Authorized');
+            err.title = 'Not Authorized';
+            err.status = 401;
+            return next(err);
+        }
+    } else {
+        const err = new Error("Comment couldn't be found");
+        err.title = "Comment couldn't be found";
+        err.status = 404;
+        return next(err);
+    }
+});
+
 module.exports = router;
