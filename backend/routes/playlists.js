005b434ae3667d62763014671d34588363093638
Jan <janjovellanos@gmail.com>
2022-05-21 13:31:42 -0700

    success

--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -3,7 +3,7 @@ const { check } = require('express-validator');
 
 const { requireAuth, restoreUser } = require('../utils/auth');
 const { handleValidationErrors } = require('../utils/validation');
-const { Song, User, Album, Comment, Playlist } = require('../db/models');
+const { Song, User, Album, Comment, Playlist, PlaylistSong } = require('../db/models');
 
 const router = express.Router();
 
@@ -25,6 +25,45 @@ router.post('/', requireAuth, validatePlaylistCreation, async (req, res, next) =
         imageUrl
     })
     res.json(newPlaylist);
+});
+
+//add song to playlist
+router.post('/:playlistId', requireAuth, async (req, res, next) => {
+    let { playlistId } = req.params;
+    const { songId } = req.body;
+    const { user } = req;
+    playlistId = parseInt(playlistId)
+
+    const playlist = await Playlist.findByPk(playlistId);
+    const song = await Song.findByPk(songId);
+
+    if (playlist) {
+        if (song) {
+            if (user.id === playlist.userId) {
+                const playlistSongAssociation = await PlaylistSong.create({ playlistId, songId });
+                const playlistSong = await PlaylistSong.findOne({
+                    where: { playlistId, songId },
+                    attributes: ['id', 'playlistId', 'songId']
+                })
+                res.json(playlistSong);
+            } else {
+                const err = new Error('Not Authorized');
+                err.title = 'Not Authorized';
+                err.status = 401;
+                return next(err);
+            }
+        } else {
+            const err = new Error("Song couldn't be found");
+            err.title = "Song couldn't be found";
+            err.status = 404;
+            return next(err);
+        }
+    } else {
+        const err = new Error("Playlist couldn't be found");
+        err.title = "Playlist couldn't be found";
+        err.status = 404;
+        return next(err);
+    }
 })
 
 module.exports = router;
