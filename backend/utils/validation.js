32f20c5a1980fa45324c4d429459d106188e4ab9
Jan <janjovellanos@gmail.com>
2022-05-23 19:16:14 -0700

    updated errors

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -3,13 +3,18 @@ const { validationResult } = require('express-validator');
 const handleValidationErrors = (req, _res, next) => {
     const validationErrors = validationResult(req);
 
+    let errObj = {}
     if (!validationErrors.isEmpty()) {
         const errors = validationErrors
             .array()
-            .map((error) => `${error.msg}`);
+            // .map((error) => `${error.msg}`);
+            .forEach((err) => {
+                errObj[err.param] = error.msg
+            });
+
 
         const err = Error('Bad Request.');
-        err.errors = errors;
+        err.errors = errObj;
         err.status = 400;
         err.title = 'Bad request.';
         next(err);
