30778e497e811c3e467ffb4a172c843493973b1b
Jan <janjovellanos@gmail.com>
2022-05-26 20:35:48 -0700

    updated

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
-const sessionRouter = require('./session');
-const usersRouter = require('./users');
 
-router.use('/session', sessionRouter);
-router.use('/users', usersRouter);
+
+
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
