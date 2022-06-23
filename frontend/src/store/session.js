a0b7a4964b9350a0986d95d31f55f832be32f2ba
Jan <janjovellanos@gmail.com>
2022-06-22 20:34:25 -0700

    able to update state with login

---
+++ frontend/src/store/session.js
@@ -0,0 +1,58 @@
+// frontend/src/store/session.js
+import { csrfFetch } from './csrf';
+
+
+//action types
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
+
+//action creators
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user,
+    };
+};
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    };
+};
+
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data));
+    // console.log(data)
+    return response;
+};
+
+
+//session reducer
+const initialState = { user: null };
+
+const sessionReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
