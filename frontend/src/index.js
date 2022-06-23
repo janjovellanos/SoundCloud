ee67a10a675f32f911dd7953e02662007df5efbb
Jan <janjovellanos@gmail.com>
2022-06-22 20:13:35 -0700

    finished phase 0 authenticate front

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,14 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
