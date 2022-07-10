c9255e06fa0db4da5e5d49c06e72d4def317b57c
Jan <janjovellanos@gmail.com>
2022-07-10 16:57:45 -0700

    create album successful

--- frontend/src/components/Navigation/Home.js
+++ frontend/src/components/Navigation/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className='home-container'>
             <h1>
-                Explore the sounds of today
+                Explore the sounds of today,
             </h1>
             <h1>
                 Create the sounds of tomorrow
