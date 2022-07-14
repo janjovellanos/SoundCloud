94354bc502f7e7681160cb5ac9a8e205058fc0d5
Jan <janjovellanos@gmail.com>
2022-07-14 14:34:43 -0700

    successful upload

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,6 +3,7 @@ module.exports = {
     environment: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 8000,
     dbFile: process.env.DB_FILE,
+    AWS_SDK_LOAD_CONFIG: 1,
     jwtConfig: {
         secret: process.env.JWT_SECRET,
         expiresIn: process.env.JWT_EXPIRES_IN
